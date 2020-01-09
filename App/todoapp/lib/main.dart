51260a5918c1110e3cf214d516f110ee2fd22b70
Eduardo579 <luiseduardogarciadepablo@protonmail.com>
2020-01-09 11:18:32 +0100

    Crear cuadro / barra superior + botón #2

--- App/todoapp/lib/main.dart
+++ App/todoapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'modelos/global.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
       title: 'Notes',
       theme: ThemeData(
         
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
       ),
       home: MyHomePage(title: 'Notes'),
     );
@@ -35,36 +36,71 @@ class _MyHomePageState extends State<MyHomePage> {
           child: new DefaultTabController(
           length: 3,
           child: new Scaffold(
-            body: TabBarView(
-              children: [
-                new Container(
-                  color: Colors.yellow,
+            body: Stack(
+              children: <Widget>[
+               TabBarView(
+                  children: [
+                    new Container(
+                      color: grisOscuro,
+                    ),
+                    new Container(color: Colors.orange,),
+                    new Container(
+                      color: Colors.lightGreen,
+                    ),
+                  ],
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 50),
+                height: 160,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(50),
+                    bottomRight: Radius.circular(50)
+                  ),
+                  color: Colors.white,
                 ),
-                new Container(color: Colors.orange,),
-                new Container(
-                  color: Colors.lightGreen,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text("Intray", style: tituloIntray,),
+                    Container()
+                  ],
                 ),
-              ],
+              ),
+              Container(
+                height: 80,
+                width: 80,
+                margin: EdgeInsets.only(top: 120, left: MediaQuery.of(context).size.width*0.5 - 40),
+                child: FloatingActionButton(
+                  child: Icon(Icons.add, size: 60,),
+                  backgroundColor: rojo,
+                  onPressed: (){},
+                  ),
+              )
+              ]
             ),
-            appBar: new TabBar(
-              tabs: [
-                Tab(
-                  icon: new Icon(Icons.home),
-                ),
-                Tab(
-                  icon: new Icon(Icons.rss_feed),
-                ),
-                Tab(
-                  icon: new Icon(Icons.perm_identity),
-                ),
-              ],
-              labelColor: Colors.yellow,
-              unselectedLabelColor: Colors.blue,
-              indicatorSize: TabBarIndicatorSize.label,
-              indicatorPadding: EdgeInsets.all(5.0),
-              indicatorColor: Colors.red,
+            appBar: AppBar(
+              elevation: 0,
+              title: new TabBar(
+                tabs: [
+                  Tab(
+                    icon: new Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: new Icon(Icons.perm_identity),
+                  ),
+                ],
+                labelColor: grisOscuro,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+              ),
+              backgroundColor: Colors.white,
             ),
-            backgroundColor: Colors.black,
+            backgroundColor: Colors.white,
           ),
         )
       )
---
+++ App/todoapp/lib/modelos/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color grisOscuro = new Color(0xFF212128);
+Color rojo = new Color(0xFFDC4F64);
+
+TextStyle tituloIntray = new TextStyle(
+  fontFamily: 'Avenir', 
+  fontWeight: FontWeight.bold, 
+  color: grisOscuro, 
+  fontSize: 50);
