d24ed5e3aa7a939d26e5cc8e296a7ec5b00bc467
Eduardo579 <luiseduardogarciadepablo@protonmail.com>
2020-01-14 11:07:59 +0100

    Solucionado el archivo .gitignore

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-from flask_restful import Api
-from resources.Registro import Registro
-
-api_bp = Blueprint('api', __name__)
-api = Api(api_bp)
-
-api.add_resource(Registro, '/register')
